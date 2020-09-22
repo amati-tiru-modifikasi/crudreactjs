e4b87063e16f9a5234f6da7fcf7ced4416a610fb
mdestafadilah <desta.08b@gmail.com>
2020-09-22 12:49:16 +0700

    Komponen Form

---
+++ src/components/FormComponent.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { Row, Col, Label, Input, FormGroup, Button } from "reactstrap";
+
+// redux
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+
+const renderField = ({
+  input,
+  type,
+  placeholder,
+  label,
+  disabled,
+  readOnly,
+  meta: { touched, error, warning }
+}) => (
+  <Row>
+    <Col md="12">
+      <Label htmlFor="{input}" className="col-form-label">
+        {label}
+      </Label>
+    </Col>
+    <Col md="12">
+      <Input
+        {...input}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        readOnly={readOnly}
+      ></Input>
+      {touched &&
+        ((error && <p style={{ color: "red" }}>{error}</p>) ||
+          (warning && <p style={{ color: "brown" }}>{warning}</p>))}
+    </Col>
+  </Row>
+);
+
+let FormComponent = props => {
+  return (
+    <form>
+      <FormGroup>
+        <Col md={6}>
+          <FormGroup>
+            <Field
+              type="text"
+              name="nama"
+              component={renderField}
+              label="nama"
+            />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Field
+              type="text"
+              name="alamat"
+              component={renderField}
+              label="alamat"
+            />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Field
+              type="text"
+              name="nohp"
+              component={renderField}
+              label="nohp"
+            />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Field
+              type="text"
+              name="umur"
+              component={renderField}
+              label="umur"
+            />
+          </FormGroup>
+        </Col>
+      </FormGroup>
+
+      <FormGroup row>
+        <Col md="12">
+          <FormGroup>
+            <Button color="dark" type="submit" disabled={props.submitting}>
+              Submit
+            </Button>
+          </FormGroup>
+        </Col>
+      </FormGroup>
+    </form>
+  );
+};
+
+FormComponent = reduxForm({
+  form: "formCreateUser",
+  enableReinitialize: true
+})(FormComponent);
+
+export default connect()(FormComponent);
--- src/containers/CreateUserContainer.js
+++ src/containers/CreateUserContainer.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { Container } from "reactstrap";
+import FormComponent from "../components/FormComponent";
 
 class CreateUserContainer extends Component {
   render() {
-    return <Container>Create user</Container>;
+    return (
+      <Container>
+        Create user
+        <FormComponent />
+      </Container>
+    );
   }
 }
 
